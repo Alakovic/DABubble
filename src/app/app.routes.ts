c19557c65874dc7299114f7036536cef7ff300c9
Zeljko Alakovic <zeljkoalakovic997@outlook.com>
2026-05-17 16:48:51 +0200

    login UI finished

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { Login } from './shared/login/login';
+import { Register } from './shared/register/register';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        component: Login
+    },
+    {
+        path: 'sign-in',
+        component:Register 
+    }
+];
---
+++ src/app/shared/login/login.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon';
+import { MatDividerModule } from '@angular/material/divider';
+
+@Component({
+  selector: 'app-login',
+  imports: [MatCardModule, MatFormFieldModule, MatInputModule, MatIconModule,  MatDividerModule],
+  templateUrl: './login.html',
+  styleUrls: ['./login.scss'] ,
+})
+export class Login {}

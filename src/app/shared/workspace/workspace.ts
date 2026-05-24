07088e87e0d338562af9eefe2d94f0548cf9efea
Zeljko Alakovic <zeljkoalakovic997@outlook.com>
2026-05-24 18:24:43 +0200

    added sidebar , thread and main UI.. sidebar togggle active

--- src/app/shared/workspace/workspace.ts
+++ src/app/shared/workspace/workspace.ts
@@ -1,26 +1,27 @@
-import { Component, computed, inject, signal } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
 import { Router } from '@angular/router';
 import { UserService } from '../../services/user.service';
 import { AuthService } from '../../services/auth.service';
-import { UserInterface } from '../../interfaces/user-interface';
 import { MatMenuModule } from '@angular/material/menu';
-import { MatDialog } from '@angular/material/dialog';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDialog, MatDialogModule } from '@angular/material/dialog';
 import { UserProfile } from '../user-profile/user-profile';
 
 @Component({
   selector: 'app-workspace',
-  imports: [MatIconModule, MatMenuModule],
+  imports: [MatIconModule, MatMenuModule, MatButtonModule, MatDialogModule],
   templateUrl: './workspace.html',
-  styleUrls: ['./workspace.scss'] ,
+  styleUrls: ['./workspace.scss'],
 })
 export class Workspace {
   authService = inject(AuthService);
   userService = inject(UserService);
- 
+  sidebarOpen = signal(false);
+  threadOpen = signal(true);
   isMenuOpen = signal(false);
   router = inject(Router);
-   dialog = inject(MatDialog);
+  dialog = inject(MatDialog);
 
   async ngOnInit() {
     await this.userService.getAllUsers();
@@ -35,18 +36,20 @@ export class Workspace {
 
   async logout() {
     const firebaseUser = this.authService.auth.currentUser;
-    if(firebaseUser){
-      await this.userService.updateUserStatus(
-        firebaseUser.uid, 'offline'
-      );
+    if (firebaseUser) {
+      await this.userService.updateUserStatus(firebaseUser.uid, 'offline');
       await this.authService.logout();
       this.router.navigate(['']);
     }
   }
 
   goToProfile() {
-    this.dialog.open(UserProfile,{
+    this.dialog.open(UserProfile, {
       data: this.userService.loggedUser(),
-    })
+    });
+  }
+
+  toggleSidebar() {
+    this.sidebarOpen.update((value) => !value);
   }
 }
